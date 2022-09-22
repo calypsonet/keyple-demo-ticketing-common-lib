60a1318693a89722147b49f3d278fb1cd688eecb
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2022-09-22 15:45:43 +0200

    feat: add card application constants

--- src/main/kotlin/org/calypsonet/keyple/demo/common/constant/CardConstant.kt
+++ src/main/kotlin/org/calypsonet/keyple/demo/common/constant/CardConstant.kt
@@ -12,6 +12,20 @@
 package org.calypsonet.keyple.demo.common.constant
 
 object CardConstant {
+  const val AID_1TIC_ICA_1 = "315449432e49434131"
+  const val AID_1TIC_ICA_3 = "315449432E49434133"
+  const val AID_NORMALIZED_IDF = "A0000004040125090101"
+  const val AID_OTHER = "Other"
+
+  const val SFI_ENVIRONMENT_AND_HOLDER = 0x07.toByte()
+  const val SFI_EVENTS_LOG = 0x08.toByte()
+  const val SFI_CONTRACTS = 0x09.toByte()
+  const val SFI_COUNTER = 0x19.toByte()
+
+  const val DEFAULT_KIF_PERSONALIZATION = 0x21.toByte()
+  const val DEFAULT_KIF_LOAD = 0x27.toByte()
+  const val DEFAULT_KIF_DEBIT = 0x30.toByte()
+
   const val ENVIRONMENT_HOLDER_RECORD_SIZE_BYTES = 29
   const val CONTRACT_RECORD_SIZE_BYTES = 29
   const val EVENT_RECORD_SIZE_BYTES = 29
