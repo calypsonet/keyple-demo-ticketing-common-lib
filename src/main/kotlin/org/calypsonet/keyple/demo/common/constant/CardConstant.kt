770770239c00ad4b59c9e920bcfb8a4b9808a41e
Andrei Cristea <andrei.cristea019@gmail.com>
2022-09-22 16:46:42 +0200

    feat: add some AIDs to CardConstant

--- src/main/kotlin/org/calypsonet/keyple/demo/common/constant/CardConstant.kt
+++ src/main/kotlin/org/calypsonet/keyple/demo/common/constant/CardConstant.kt
@@ -12,8 +12,12 @@
 package org.calypsonet.keyple.demo.common.constant
 
 object CardConstant {
+
   const val AID_1TIC_ICA_1 = "315449432e49434131"
   const val AID_1TIC_ICA_3 = "315449432E49434133"
+  const val AID_CALYPSO_LIGHT_CL = "315449432E49434133"
+  const val AID_CD_LIGHT_GTML = "315449432E49434131"
+  const val AID_NAVIGO_2013 = "A00000040401250901"
   const val AID_NORMALIZED_IDF = "A0000004040125090101"
   const val AID_OTHER = "Other"
 
